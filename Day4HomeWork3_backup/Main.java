d694c289888f9162e0b3c404f85fc9aeb6297cd2
YasinCelikSoftware <107568842+YasinCelikSoftware@users.noreply.github.com>
2022-06-15 19:03:56 +0300

    Update Main.java

--- Day4HomeWork3_backup/Main.java
+++ Day4HomeWork3_backup/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		Member member = new Member(1, "Yasin", "Celik","1998" , "39016795112");
+		Member member = new Member(1, "Yasin", "Celik","1998" , "39016795127");
 		Game game = new Game("Valorant");
 		Game riseOnline = new Game("Rise Online");
 		Offer offer = new Offer("Yaz Indirimi", 25);
@@ -25,7 +25,7 @@ public class Main {
 		offerManager.addOffer(riseOnline, offer);
 		
 		EDevletManager eDevletManager = new EDevletManager();
-		eDevletManager.realityCheck("Yasin", "Celik", "1998", "39016795112", member);
+		eDevletManager.realityCheck("Yasin", "Celik", "1998", "39016795127", member);
 		
 	}
 
